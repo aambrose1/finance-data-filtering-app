fd350d1dc59024135bc13bed6382ad64b272b026
Andrea Ambrose <andreamoesha@gmail.com>
2025-01-11 16:11:22 -0500

    Installed react-vite packages

---
+++ react-fastapi/front-end/finance-app/src/App.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Apple Inc.</h1>
+      <p>AAPL (United States: NASDAQ) </p>
+      <p>Income Statements</p>
+      <div className="card">
+        <button> 2020 </button>
+        <button> 2021 </button>
+        <button> 2022 </button>
+        <button> 2023 </button>
+        <button> 2024 </button>
+      </div>
+    </>
+  )
+}
+
+export default App
