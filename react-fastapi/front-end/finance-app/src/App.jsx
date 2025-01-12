28ba99d4f3749f42a7cf17c6cac2c9f22d625ef7
Andrea Ambrose <andreamoesha@gmail.com>
2025-01-12 08:04:49 -0500

    Basic styling added

--- react-fastapi/front-end/finance-app/src/App.jsx
+++ react-fastapi/front-end/finance-app/src/App.jsx
@@ -1,22 +1,24 @@
+/* eslint-disable react/no-unknown-property */
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Apple Inc.</h1>
-      <p>AAPL (United States: NASDAQ) </p>
-      <p>Income Statements</p>
-      <div className="card">
-        <button> 2020 </button>
-        <button> 2021 </button>
-        <button> 2022 </button>
-        <button> 2023 </button>
-        <button> 2024 </button>
+    <div class="font-sans whitespace-pre-wrap">
+      <h1 class="text-4xl">Apple Inc.</h1>
+      <p class = "italic">AAPL (United States: NASDAQ) </p>
+      <p>Annual Income Statements</p>
+      <span class="p-2 h-18 w-30 inline-block align-middle flex space-x-5 items-center justify-center">
+        <button type="button" class="border-2 rounded-full hover:bg-slate-200"> 2020 </button>
+        <button type="button" class="border-2 rounded-full hover:bg-slate-200"> 2021 </button>
+        <button type="button" class="border-2 rounded-full hover:bg-slate-200"> 2022 </button>
+        <button type="button" class="border-2 rounded-full hover:bg-slate-200"> 2023 </button>
+        <button type="button" class="border-2 rounded-full hover:bg-slate-200"> 2024 </button>
+      </span>
       </div>
     </>
   )
--- react-fastapi/front-end/finance-app/src/Table.jsx
+++ react-fastapi/front-end/finance-app/src/Table.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable react/jsx-key */
 import * as React from "react";
 import {useState, useEffect } from "react";
@@ -47,14 +48,19 @@ function Table() {
     const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data })
 
 	return (
-		<div>
-            <table {...getTableProps()}>
-                <thead>
+		<div class="relative overflow-x-auto w-full h-full">
+            <input type="text" class="block pt-2 ps-3 text-sm border rounded-lg w-80 bg-gray-50 flex items-center" placeholder="Search for items"></input>
+            <table class=" text-left text-slate-800 border-2 rounded-full" {...getTableProps()}>
+                <thead class="text-s  border">
                     {headerGroups.map((headerGroup) => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
+                        <tr class="text-slate-500" {...headerGroup.getHeaderGroupProps()}>
                             {headerGroup.headers.map((column) => (
-                                <th {...column.getHeaderProps()}>
+                                <th class="flex-row justify-around items-center"{...column.getHeaderProps()}>
+                                    
                                     {column.render("Header")}
+                                    <a href="#"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                        <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                                    </svg></a>
                                 </th>
                             ))}
                         </tr>
@@ -64,9 +70,9 @@ function Table() {
                     {rows.map((row) => {
                         prepareRow(row)
                         return (
-                            <tr {...row.getRowProps()}>
+                            <tr  class="hover:bg-slate-200" {...row.getRowProps()}>
                                 {row.cells.map((cell) => (
-                                    <td {...cell.getCellProps()}>
+                                    <td class ="px-2" {...cell.getCellProps()}>
                                         {cell.render("Cell")}
                                     </td>
                                 ))}
