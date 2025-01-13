6542c1dfe115543d395d1e83eef5a9ca5f80a614
Andrea Ambrose <andreamoesha@gmail.com>
2025-01-13 01:38:40 -0500

    Added filters and table

--- react-fastapi/back-end/main.py
+++ react-fastapi/back-end/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 import httpx
+from pydantic import BaseModel, model_validator
+import json
 
 app = FastAPI()
 
@@ -17,11 +19,15 @@ app.add_middleware(
     allow_headers = ["*"]
 )
 
-# List of dictionaries of data from 2021 to 2024
+# # List of dictionaries of data from 2021 to 2024
 API_URL = "https://financialmodelingprep.com/api/v3/income-statement/AAPL?period=annual&apikey="
-API_KEY = "8bMDrZT7MhS3C6sNzLeujKMlfedx5vIM"
+API_KEY = "UdVNPWNCQOUi2gCvEdB7tcT71kJDfnqw"
 
 async def fetch_data(request: Request):
+    # UNCOMMENT IF ERR 429. API KEY REQUESTS ARE LIMITED.
+    # with open("AAPL-APIDATA.json") as f:
+    #     return json.load(f)
+
     url = f"{API_URL}{API_KEY}"
 
     try:
@@ -33,17 +39,65 @@ async def fetch_data(request: Request):
         else:
             raise HTTPException(status_code=response.status_code, detail=f"Failure fetching data")
     
-    except:
+    except Exception as e:
+        print(e)
         raise HTTPException(status_code=500, detail="An internal error has occured.")
 
 
-#TO-DO : Make data filters. Years: Buttons, Net: Search, Rev: Search
+
 @app.get("/display_data/")
 async def display_data(request: Request):
     dataSet = await fetch_data(request)
-    data2024 = dataSet[0]
-    data2023 = dataSet[1]
-    data2022 = dataSet[2]
-    data2021 = dataSet[3]
-    data2020 = dataSet[4]
     return dataSet
+
+
+@app.get("/year/{year}/")
+async def year(request: Request, year = None):
+    dataSet = await fetch_data(request)
+    year = int(year)
+    if year != None:
+        match year:
+            case 0:
+                dataSet = dataSet[0] #2024
+                return [dataSet]
+            case 1:
+                dataSet = dataSet[1] #2023
+                return [dataSet]
+            case 2:
+                dataSet = dataSet[2] #2022
+                return [dataSet]
+            case 3:
+                dataSet = dataSet[3] #2021
+                return [dataSet]
+            case 4:
+                dataSet = dataSet[4] #2020
+                return [dataSet]
+    return dataSet
+
+class FilterOptions(BaseModel):
+    min: int
+    max: int
+
+    @model_validator(mode='before')
+    @classmethod
+    def validate_to_json(cls, value):
+        print(value)
+        # print(isinstance(value, str))
+        # print(type(value))
+        if isinstance(value, bytes):
+            value = value.decode()
+        if isinstance(value, str):
+            return cls(**json.loads(value))
+        return value
+
+@app.post("/filter_revenue/")
+async def filter_revenue(request: Request, item: FilterOptions):
+    dataSet = await fetch_data(request)
+    filteredSet = filter(lambda dict: dict['revenue'] >= item.min and dict['revenue'] <= item.max, dataSet)
+    return list(filteredSet)
+
+@app.post("/filter_income/")
+async def filter_income(request: Request, item: FilterOptions):
+    dataSet = await fetch_data(request)
+    filteredSet = filter(lambda dict: dict['netIncome'] >= item.min and dict['netIncome'] <= item.max, dataSet)
+    return list(filteredSet)
--- react-fastapi/front-end/finance-app/src/Table.jsx
+++ react-fastapi/front-end/finance-app/src/Table.jsx
@@ -2,10 +2,12 @@
 /* eslint-disable react/jsx-key */
 import * as React from "react";
 import {useState, useEffect } from "react";
-import { useTable } from "react-table";
+import { useTable, useSortBy } from "react-table";
 
 function Table() {
     const [statements, setStatements] = useState([])
+    const [revenueFilterValues, setRevenueFilterValues] = useState({min: 0, max: 999999999999999})
+    const [incomeFilterValues, setIncomeFilterValues] = useState({min: 0, max: 999999999999999})
 
     const fetchData = async () => {
 		const response = await fetch("http://127.0.0.1:8000/display_data/");
@@ -16,6 +18,70 @@ function Table() {
 	useEffect(() => {
 		fetchData();
 	}, []);
+
+    const handleClick = async (value) => {
+        const response = await fetch(`http://127.0.0.1:8000/year/${value}`);
+		const APIdata = await response.json();
+        setStatements(APIdata);
+    }
+
+    const handleRevenueFilterMin = async (min) => {
+        setRevenueFilterValues((previousDict) => {
+            if (!isNaN(parseInt(min))){
+                return {min: parseInt(min), max: previousDict.max}
+            }
+            return {min: 0, max: previousDict.max};
+        });
+    }
+
+    const handleRevenueFilterMax = async (max) => {
+        setRevenueFilterValues((previousDict) => {
+            if (!isNaN(parseInt(max))){
+                return {min: previousDict.min, max: parseInt(max)}
+            }
+            return {min: previousDict.min, max: 999999999999999};
+        });
+    }
+
+    useEffect(() => {
+        const filterFun = async () => {
+           const response = await fetch("http://127.0.0.1:8000/filter_revenue/", {
+            method: "POST", body: JSON.stringify(revenueFilterValues)
+        });
+        const APIdata = await response.json();
+        setStatements(APIdata); 
+        }
+        filterFun()
+    }, [revenueFilterValues])
+
+    const handleIncomeFilterMin = async (min) => {
+        setIncomeFilterValues((previousDict) => {
+            if (!isNaN(parseInt(min))){
+                return {min: parseInt(min), max: previousDict.max}
+            }
+            return {min: 0, max: previousDict.max};
+        });
+    }
+
+    const handleIncomeFilterMax = async (max) => {
+        setIncomeFilterValues((previousDict) => {
+            if (!isNaN(parseInt(max))){
+                return {min: previousDict.min, max: parseInt(max)}
+            }
+            return {min: previousDict.min, max: 999999999999999};
+        });
+    }
+
+    useEffect(() => {
+        const filterFun = async () => {
+           const response = await fetch("http://127.0.0.1:8000/filter_income/", {
+            method: "POST", body: JSON.stringify(incomeFilterValues)
+        });
+        const APIdata = await response.json();
+        setStatements(APIdata); 
+        }
+        filterFun()
+    }, [incomeFilterValues])
 
     const data = React.useMemo(() => statements, [statements])
     const columns = React.useMemo(() => [
@@ -45,43 +111,96 @@ function Table() {
         }
     ], []);
 
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data })
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data }, useSortBy);
 
 	return (
-		<div class="relative overflow-x-auto w-full h-full">
-            <input type="text" class="block pt-2 ps-3 text-sm border rounded-lg w-80 bg-gray-50 flex items-center" placeholder="Search for items"></input>
-            <table class=" text-left text-slate-800 border-2 rounded-full" {...getTableProps()}>
-                <thead class="text-s  border">
-                    {headerGroups.map((headerGroup) => (
-                        <tr class="text-slate-500" {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) => (
-                                <th class="flex-row justify-around items-center"{...column.getHeaderProps()}>
-                                    
-                                    {column.render("Header")}
-                                    <a href="#"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                                        <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
-                                    </svg></a>
-                                </th>
+            <><div class=" px-6 pt-6 pb-2 rounded-xl shadow-lg">
+                <div class="font-sans-serif whitespace-pre-wrap">
+                <h1 class="text-4xl">Apple Inc.</h1>
+                <p class="italic">AAPL (United States: NASDAQ) </p>
+                <p>Annual Income Statements</p>
+                <span class="p-2 h-18 w-30 inline-block align-middle flex space-x-5 items-center justify-center">
+                    <button type="button" value="4" onClick={(e) => handleClick(e.target.value)} class="border-2 rounded-full hover:bg-slate-100"> 2020 </button>
+                    <button type="button" value="3" onClick={(e) => handleClick(e.target.value)}  class="border-2 rounded-full hover:bg-slate-100"> 2021 </button>
+                    <button type="button" value="2" onClick={(e) => handleClick(e.target.value)}  class="border-2 rounded-full hover:bg-slate-100"> 2022 </button>
+                    <button type="button" value="1" onClick={(e) => handleClick(e.target.value)}  class="border-2 rounded-full hover:bg-slate-100"> 2023 </button>
+                    <button type="button" value="0" onClick={(e) => handleClick(e.target.value)}  class="border-2 rounded-full hover:bg-slate-100"> 2024 </button>
+                </span>
+            </div>
+            <div class="relative overflow-x-auto w-full h-full">
+                <div class="">
+                        <p>Filter by Revenue</p>
+                        <div class="flex">
+                        <input
+                            class="w-13 h-5 rounded"
+                            size="15"
+                            type="text"
+                            placeholder="Min"
+                            onChange={(e) => handleRevenueFilterMin(e.target.value)}
+                        /> 
+                        <p class="">-</p>
+                        <input
+                            class="w-13 h-5 rounded"
+                            size="15"
+                            type="text"
+                            placeholder="Max"
+                            onChange={(e) => handleRevenueFilterMax(e.target.value)}
+                        /> 
+                    </div>
+                     <p>Filter by Net Income</p>
+                    <div class="flex">
+                        <input
+                            class="w-13 h-5 rounded"
+                            size="15"
+                            type="text"
+                            placeholder="Min"
+                            onChange={(e) => handleIncomeFilterMin(e.target.value)}
+                        /> 
+                       <p class="">-</p>
+                        <input
+                            class="w-13 h-5 rounded"
+                            size="15"
+                            type="text"
+                            placeholder="Max"
+                            onChange={(e) => handleIncomeFilterMax(e.target.value)}
+                        /> 
+                    </div>
+                </div>
+                    <table class=" mx-auto text-left text-slate-800 rounded border bg-white p-10" {...getTableProps()}>
+                        <thead class="text-s border">
+                            {headerGroups.map((headerGroup) => (
+                                <tr class="text-slate-500 " {...headerGroup.getHeaderGroupProps()}>
+                                    {headerGroup.headers.map((column) => (
+                                        <th class="flex-row justify-around items-center hover:bg-slate-200" {...column.getHeaderProps(column.getSortByToggleProps)}>
+
+                                            {column.render("Header")}
+                                            <span>
+                                                {column.isSorted ? (column.isSorted ? ">" : "<") : ""}
+                                            </span>
+                                        </th>
+                                    ))}
+                                </tr>
                             ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {rows.map((row) => {
-                        prepareRow(row)
-                        return (
-                            <tr  class="hover:bg-slate-200" {...row.getRowProps()}>
-                                {row.cells.map((cell) => (
-                                    <td class ="px-2" {...cell.getCellProps()}>
-                                        {cell.render("Cell")}
-                                    </td>
-                                ))}
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-        </div>
+                        </thead>
+                        <tbody {...getTableBodyProps()}>
+                            {rows.map((row) => {
+                                prepareRow(row);
+                                return (
+                                    <tr class="hover:bg-slate-200" {...row.getRowProps()}>
+                                        {row.cells.map((cell) => (
+                                            <td class="px-2" {...cell.getCellProps()}>
+                                                {cell.render("Cell")}
+                                            </td>
+                                        ))}
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                    <a href="/"><p class="my-2 italic text-xs uppercase hover:underline"> Refresh to restart </p></a>
+                </div>
+            </div>
+        </>
 	);
 }
 
